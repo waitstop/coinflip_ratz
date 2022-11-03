31208dff11ef405f08c12d388aab4cc33086567a
emtyxd <emtyxd@mail.ru>
2022-11-04 00:06:09 +0300

    wallet updates

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
 
 export const BD_URI = process.env.NODE_ENV !== 'production' ? "http://localhost:5000/graphql":"https://37.140.192.155:5000/graphql"
-export const PUBLIC_ADDRESS_OWNER = "AvwiLmqEzJ3sQrWWAaq2ADEgrNywtnzig3FrWcAcuqPB"
+export const PUBLIC_ADDRESS_OWNER = "AHyZ8fr4JgXcqnCBHQAkHtiABq4EQkmm7ZPcnr7rekgX"
 export const network = process.env.NODE_ENV !== 'production' ? WalletAdapterNetwork.Devnet: WalletAdapterNetwork.Devnet; //CHANGE IF PROD
